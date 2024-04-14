4baa6f76b428098fd1410aea90a6ffb68c234a82
sundisaab <166897100+sundisaab@users.noreply.github.com>
2024-04-14 11:01:53 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,12 @@
+import pywhatkit as pwk
+ 
+ 
+try:
+      
+     pwk.sendwhatmsg("+91 6266977655", "lwde", 13,32)
+ 
+     print("Message Sent!")  
+ 
+     
+except: 
+     print("Error in sending the message")
